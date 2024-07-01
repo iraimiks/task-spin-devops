ba7f8eaeccf2210eddc05247aabdc8489c12ee1f
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2024-07-01 18:00:32 +0300

    add app for stress test

--- testing/script.js
+++ testing/script.js
@@ -2,15 +2,12 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  stages: [
-  { duration: '10m', target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
-  { duration: '30m', target: 200 }, // stay at higher 200 users for 30 minutes
-  { duration: '5m', target: 0 }, // ramp-down to 0 users
-  ]
-}
+  vus: 100,
+  duration: '30s',
+};
 
 
 export default function() {
-  http.get('http://localhost:56957/');
+  http.get('http://localhost:57624/');
   sleep(1);
 }
